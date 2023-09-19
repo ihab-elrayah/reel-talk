fda1ccdf253469ecfac788db66d029a103e0333f
ronnydeng67 <ronnydeng67@gmail.com>
2023-09-19 12:52:14 -0400

    able to prompt to gmail login

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const passport = require('passport');
 const app = express();
 
+require('./auth')
+
 app.get("/", (req, res) => {
-    res.json("Hello from backend")
+    res.send('<a href="/auth/google">Authenticate with Google</a>')
+})
+
+app.get('/auth/google', 
+    passport.authenticate('google', { scope: ['email', 'profile'] })
+)
+
+app.get("/protected", (req, res) => {
+    res.send("Welcome")
 })
 
 app.listen(8080, () => {
---
+++ backend/auth.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const dotenv = require('dotenv')
+dotenv.config();
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:8080/auth/google/callback",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //   return done(err, user);
+    // });
+    return done(err, profile)
+  }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user)
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user)
+});
