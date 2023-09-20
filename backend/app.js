5c620dc9410ab6a58bde1169a52b0dd1e2d960e1
ronnydeng67 <ronnydeng67@gmail.com>
2023-09-20 12:53:08 -0400

    clean up app.js

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,6 @@ app.use(passport.session());
 
 const authRoute = require('./routes/auth');
 
-// function isLoggedIn(req, res, next) {
-//     req.user ? next() : res.sendStatus(401);
-// }
 
 app.get("/", (req, res) => {
     res.send('<a href="/auth/google">Authenticate with Google</a>')
@@ -18,30 +15,6 @@ app.get("/", (req, res) => {
 
 app.use('/auth', authRoute)
 
-// app.get('/auth/google', 
-//     passport.authenticate('google', { scope: ['email', 'profile'] })
-// )
-
-// app.get('/auth/google/callback', 
-//     passport.authenticate('google', {
-//         successRedirect: '/protected',
-//         failureRedirect: '/auth/failure'
-//     })
-// )
-
-// app.get('/auth/failure', (req, res) => {
-//     res.send('Something went wrong!')
-// })
-
-// app.get("/protected", isLoggedIn, (req, res) => {
-//     res.send(`Welcome, ${req.user.displayName}`)
-// })
-
-// app.get('/logout', (req, res) => {
-//     req.logOut();
-//     req.session.destroy();
-//     res.send("You've successfully logout!")
-// })
 
 app.listen(8080, () => {
     console.log("Server is running on port 8080")
